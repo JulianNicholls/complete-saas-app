9a72fb4d1b6a3904fa1e1c24e8c36fa892f2498e
Julian Nicholls <juliannicholls29@gmail.com>
2015-12-10 10:42:48 +0000

    Add 'New Project' functionality
    Add bootstrap datepicker gem.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :set_tenant, except: [:index]
+  before_action :verify_tenant
 
   # GET /projects
   # GET /projects.json
@@ -26,28 +28,20 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
 
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
-        format.json { render :show, status: :created, location: @project }
-      else
-        format.html { render :new }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    if @project.save
+      redirect_to root_url, notice: 'Project was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /projects/1
   # PATCH/PUT /projects/1.json
   def update
-    respond_to do |format|
-      if @project.update(project_params)
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { render :show, status: :ok, location: @project }
-      else
-        format.html { render :edit }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    if @project.update(project_params)
+      redirect_to root_url, notice: 'Project was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -56,19 +50,30 @@ class ProjectsController < ApplicationController
   def destroy
     @project.destroy
     respond_to do |format|
-      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
+      format.html { redirect_to root_url, notice: 'Project was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_project
-      @project = Project.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_project
+    @project = Project.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def project_params
-      params.require(:project).permit(:title, :details, :expected_completion_date, :tenant_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def project_params
+    params.require(:project).permit(:title, :details, :expected_completion_date, :tenant_id)
+  end
+
+  def set_tenant
+    @tenant = Tenant.find params[:tenant_id]
+  end
+
+  def verify_tenant
+    return if params[:tenant_id] == Tenant.current_tenant_id.to_s
+
+    redirect_to :root,
+                flash: { error: 'You are not allowed to look at projects outside your organisation' }
+  end
 end
