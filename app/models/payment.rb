17bde9fe049014626a379e42aa7ee46c80b74db4
Julian Nicholls <juliannicholls29@gmail.com>
2015-12-11 15:03:31 +0000

    Add setup code for Stripe payments

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,26 @@
 class Payment < ActiveRecord::Base
-  belongs_to :tenant_id
+  attr_accessor :card_number, :card_cvv, :card_expires_month, :card_expires_year
+
+  belongs_to :tenant
+
+  def self.month_options
+    Date::MONTHNAMES.compact.each_with_index.map do |name, idx|
+      month = idx + 1
+      ["#{month} - #{name}", month]
+    end
+  end
+
+  def self.year_options
+    this_year = Date.today.year
+    (this_year..(this_year + 10)).to_a
+  end
+
+  def process_payment
+    customer = Stripe::Customer.create email: email, card: token
+
+    Stripe::Charge.create customer:     customer.id,
+                          amount:       1000,
+                          description:  'Premium Plan',
+                          currency:     'gbp'
+  end
 end
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -4,6 +4,10 @@ class Tenant < ActiveRecord::Base
   has_many :members,  dependent: :destroy
   has_many :projects, dependent: :destroy
 
+  has_one  :payment
+
+  accepts_nested_attributes_for :payment
+
   validates_presence_of :name
   validates_uniqueness_of :name
 
