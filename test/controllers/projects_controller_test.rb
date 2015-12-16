ef01f1ca3277e9c6332c42059901102e1e2d34a6
Julian Nicholls <juliannicholls29@gmail.com>
2015-12-16 16:57:32 +0000

    Complete test with Rubocop

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -5,41 +5,54 @@ class ProjectsControllerTest < ActionController::TestCase
     @project = projects(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:projects)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create project" do
+  test 'should create project' do
     assert_difference('Project.count') do
-      post :create, project: { details: @project.details, expected_completion_date: @project.expected_completion_date, tenant_id: @project.tenant_id, title: @project.title }
+      post :create,
+           project: {
+             details: @project.details,
+             expected_completion_date: @project.expected_completion_date,
+             tenant_id: @project.tenant_id,
+             title: @project.title
+           }
     end
 
     assert_redirected_to project_path(assigns(:project))
   end
 
-  test "should show project" do
+  test 'should show project' do
     get :show, id: @project
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @project
     assert_response :success
   end
 
-  test "should update project" do
-    patch :update, id: @project, project: { details: @project.details, expected_completion_date: @project.expected_completion_date, tenant_id: @project.tenant_id, title: @project.title }
+  test 'should update project' do
+    patch :update,
+          id: @project,
+          project: {
+            details: @project.details,
+            expected_completion_date: @project.expected_completion_date,
+            tenant_id: @project.tenant_id,
+            title: @project.title
+          }
     assert_redirected_to project_path(assigns(:project))
   end
 
-  test "should destroy project" do
+  test 'should destroy project' do
     assert_difference('Project.count', -1) do
       delete :destroy, id: @project
     end
