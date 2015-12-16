596fdcc6eecd7d0c8bb8746bab4be86fe3b9ea89
Julian Nicholls <juliannicholls29@gmail.com>
2015-12-16 17:04:30 +0000

    Complete initialisers with Rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -8,21 +8,26 @@ Rails.application.routes.draw do
 
   get 'home/index'
 
-  root :to => "home#index"
+  root to: 'home#index'
 
   # *MUST* come *BEFORE* devise's definitions (below)
   as :user do
     match '/user/confirmation' => 'confirmations#update',
-          :via => :put, :as => :update_user_confirmation
+          via: :put, as: :update_user_confirmation
   end
 
-  devise_for :users, :controllers => {
-    registrations:  "registrations",
-    confirmations:  "confirmations",
-    sessions:       "milia/sessions",
-    passwords:      "milia/passwords",
+  devise_for :users, controllers: {
+    registrations:  'registrations',
+    confirmations:  'confirmations',
+    sessions:       'milia/sessions',
+    passwords:      'milia/passwords'
   }
 
-  match '/plan/edit'    => 'tenants#edit', via: :get, as: :edit_plan
-  match '/plan/update'  => 'tenants#update', via: [:put, :patch], as: :update_plan
+  match '/plan/edit' => 'tenants#edit',
+        via: :get,
+        as: :edit_plan
+
+  match '/plan/update' => 'tenants#update',
+        via: [:put, :patch],
+        as: :update_plan
 end
