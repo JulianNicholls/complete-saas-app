4c80a01d587a0b9f113bb93b771a86364229776c
Julian Nicholls <juliannicholls29@gmail.com>
2015-12-16 18:57:13 +0000

    Complete helpers with Rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,38 +1,46 @@
+# App;lication helper functions
 module ApplicationHelper
-  ALERT_TYPES = [:success, :info, :warning, :danger] unless const_defined?(:ALERT_TYPES)
+  ALERT_TYPES = {
+    notice: :success,
+    info: :info,
+    warning: :warning,
+    alert: :danger,
+    error: :danger
+  } unless const_defined?(:ALERT_TYPES)
 
   def bootstrap_flash(options = {})
     flash_messages = []
     flash.each do |type, message|
-      # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
+      # Skip empty messages, e.g. devise messages set to nothing in a locale.
       next if message.blank?
 
-      type = type.to_sym
-      type = :success if type == :notice
-      type = :danger  if type == :alert
-      type = :danger  if type == :error
-      next unless ALERT_TYPES.include?(type)
+      type = ALERT_TYPES.fetch(type.to_sym) { next }
 
       tag_class = options.extract!(:class)[:class]
       tag_options = {
         class: "alert fade in alert-#{type} #{tag_class}"
       }.merge(options)
 
-      close_button = content_tag(:button, raw("&times;"), type: "button", class: "close", "data-dismiss" => "alert")
+      close_button = content_tag(:button,
+                                 raw('&times;'),
+                                 type: 'button',
+                                 class: 'close',
+                                 'data-dismiss' => 'alert')
 
       Array(message).each do |msg|
         text = content_tag(:div, close_button + msg, tag_options)
         flash_messages << text if msg
       end
     end
-    flash_messages.join("\n").html_safe
+
+    flash_messages.join('\n').html_safe
   end
 
   def tenant_name(tenant_id)
     Tenant.find(tenant_id).name
   end
 
-  def s3_link(teant_id, artefact_key)
+  def s3_link(_tenant_id, artefact_key)
     link_to artefact_key, artefact_key, class: 'main-link', target: 'new'
   end
 
