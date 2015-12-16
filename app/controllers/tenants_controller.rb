ecfca91c4df51d81e394aebebbfd4eb38332d01b
Julian Nicholls <juliannicholls29@gmail.com>
2015-12-16 22:12:42 +0000

    Complete tenant / reg ctlrs with Rubocop

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,3 +1,4 @@
+# Organisations controller
 class TenantsController < ApplicationController
   before_action :set_tenant
 
@@ -9,25 +10,29 @@ class TenantsController < ApplicationController
       Tenant.transaction do
         if @tenant.update(tenant_params)
           if @tenant.plan == 'premium' && @tenant.payment.blank?
-            @payment = Payment.new({
+            @payment = Payment.new(
               email: tenant_params[:email],
               token: params[:payment]['token'],
               tenant: @tenant
-            })
+            )
 
             begin
               @payment.process_payment
               @payment.save
-            rescue Exception => err
+            rescue StandardError => err
               flash[:error] = err.message
               @tenant.destroy
               @tenant.plan = 'free'
               @tenant.save
-              redirect_to tenant_edit_path(@tenant) and return
+              redirect_to tenant_edit_path(@tenant)
+              return
             end
           end
 
-          format.html { redirect_to edit_plan_path, notice: 'Your plan was successfully updated' }
+          format.html do
+            redirect_to edit_plan_path,
+                        notice: 'Your plan was successfully updated'
+          end
         else
           format.html { render :edit }
         end
@@ -39,7 +44,8 @@ class TenantsController < ApplicationController
     @tenant = Tenant.find params[:id]
     Tenant.set_current_tenant @tenant.id
     session[:tenant_id] = Tenant.current_tenant_id
-    redirect_to home_index_path, notice: "Switched to organisation #{@tenant.name}"
+    redirect_to home_index_path,
+                notice: "Switched to organisation #{@tenant.name}"
   end
 
   private
